848861c4ddbf81032e41305b3f88ab8f274c5bab
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-07-17 10:03:41 +0900

    post, postmanager 수정

---
+++ berryberrymarket/src/berryberrymarket/Post.java
@@ -0,0 +1,17 @@
+package berryberrymarket;
+
+public class Post {
+
+	private String title;
+	private String nickname;
+	private String content;
+	private int price;
+	private String place;
+	private String date;
+
+	public void printInfo() {
+		
+	}
+	
+
+}
