9ff7d273618c04089c83f7a4c3f9582ca0624f58
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-07-19 16:02:23 +0900

    맨 위 소캣관련 주석풀다 다시 주석걸음

--- berryberrymarket/src/berryberrymarket/BerryBerrymarketApp.java
+++ berryberrymarket/src/berryberrymarket/BerryBerrymarketApp.java
@@ -24,7 +24,7 @@ public class BerryBerrymarketApp {
 //                e.printStackTrace();
 //            }
 //        }).start();
-        /////////////////////////////////////////
+       
         
 		PrintPage pp = new PrintPage();
 		int pageCase = 2;
