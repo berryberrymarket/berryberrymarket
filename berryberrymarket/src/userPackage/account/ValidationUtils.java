f739755ec0917f333c592bdf29fe22e7a81fb4ff
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-07-21 14:37:40 +0900

    ValidationUtil 클래스에 유효성 검사 분리, UserUpdatePage에 회원 정보 수정 기능 추가

---
+++ berryberrymarket/src/userPackage/account/ValidationUtils.java
@@ -0,0 +1,31 @@
+package userPackage.account;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ValidationUtils {
+	
+	 	private static final String SPECIAL_CHARACTERS = "!\"#$%&'()*+,-./:;?@[\\]^_`{|}~";
+	    private static final String ID_REGEX = "[a-z0-9_-]{5,20}";
+	    private static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[" + Pattern.quote(SPECIAL_CHARACTERS) + "])[a-zA-Z\\d" + Pattern.quote(SPECIAL_CHARACTERS) + "]{8,16}$";
+	    private static final String PHONE_REGEX = "^\\d{3}-\\d{4}-\\d{4}$";
+
+	    public static boolean isValidId(String id) {
+	        return id.matches(ID_REGEX);
+	    }
+
+	    public static boolean isValidPassword(String password) {
+	        return password.matches(PASSWORD_REGEX);
+	    }
+
+	    public static boolean isValidPhoneNumber(String phoneNumber) {
+	        return phoneNumber.matches(PHONE_REGEX);
+	    }
+
+	    public static boolean isValidAddress(String address) {
+	        return address.contains("시") || address.contains("군") || address.contains("구");
+	    }
+	    
+	
+	    
+}
