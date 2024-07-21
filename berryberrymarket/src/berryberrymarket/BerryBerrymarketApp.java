14cd832ee3a2b32b5c9d03c4c1711dd5840969d3
SungJin Park <pk942@naver.com>
2024-07-21 14:36:54 +0900

    로그인 회원가입 페이지 뒤로가기 기능 추가, lowerCase 추가

--- berryberrymarket/src/berryberrymarket/BerryBerrymarketApp.java
+++ berryberrymarket/src/berryberrymarket/BerryBerrymarketApp.java
@@ -22,7 +22,6 @@ public class BerryBerrymarketApp {
 		
 		PrintPage pp = new PrintPage();
 		int pageCase = 2;
-		Scanner sc = new Scanner(System.in);
 
 		try {
 			while (true) {
@@ -30,25 +29,22 @@ public class BerryBerrymarketApp {
 					System.out.println("시스템을 종료합니다.");
 					break;
 				} else if (pageCase == 1) { // 메인페이지 출력
-					pageCase = pp.printMainPage(sc);
+					pageCase = pp.printMainPage();
 				} else if (pageCase == 2) { // 로그인페이지 출력
-					pageCase = pp.printLogInPage(sc);
+					pageCase = pp.printLogInPage();
 				} else if (pageCase == 3) { // 회원가입페이지 출력
-					pageCase = pp.printSignUpPage(sc);
+					pageCase = pp.printSignUpPage();
 				} else if (pageCase == 4) { // 마이페이지 출력
-					pageCase = pp.printMyPage(sc);
+					pageCase = pp.printMyPage();
 				} else if (pageCase == 5) { // 게시글상세페이지 출력
-					pageCase = pp.printPostDetailPage(sc);
+					pageCase = pp.printPostDetailPage();
 				} else if (pageCase == 6) { // 게시글등록페이지 출력
-					pageCase = pp.printAddPostPage(sc);
+					pageCase = pp.printAddPostPage();
 				} else if (pageCase == 7) { // 채팅목록페이지 출력
-					pageCase = pp.printChatListPage(sc);
+					pageCase = pp.printChatListPage();
 				} else if (pageCase == 8) { // 채팅방페이지 출력
-					pageCase = pp.printChatRoomPage(sc);
+					pageCase = pp.printChatRoomPage();
 				}
-//				} else if (pageCase==9) {
-//					pageCase = pp.printFilteredPage(sc);
-//				}
 			}
 
 		} catch (FileNotFoundException e) {
